fb23323015e0e3599745b345e9c2a062b54e1ab3
Jacob M. Slone <jslone1993@gmail.com>
2015-01-30 22:04:47 -0500

    fixing redirect to check for forward header

--- config/http.js
+++ config/http.js
@@ -49,7 +49,7 @@ module.exports.http = {
     ],
 
     httpsRedirect: function (req, res, next) {
-      if(req.protocol == 'http') {
+      if(req.headers["x-forwarded-proto"] == 'https') {
         res.redirect('https://' + req.headers.host + req.url);
       }
       else {
